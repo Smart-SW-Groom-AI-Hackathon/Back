b498a07f0634d3ee6b6b256778f7e931d720f3c5
HP657 <junlee5070@gmail.com>
2026-01-11 01:34:03 +0900

    feat: webclient 설정

--- src/main/java/com/goorm/team3/config/WebClientConfig.java
+++ src/main/java/com/goorm/team3/config/WebClientConfig.java
@@ -17,12 +17,28 @@ public class WebClientConfig {
     @Value("${naver.api.client-secret}")
     private String clientSecret;
 
+    @Value("${claude-api.base-url}")
+    private String claudeBaseUrl;
+
+    @Value("${claude-api.api-key}")
+    private String claudeApiKey;
+
     @Bean
     public WebClient naverWebClient() {
         return WebClient.builder()
                 .baseUrl(baseUrl)
                 .defaultHeader("X-Naver-Client-Id", clientId)
                 .defaultHeader("X-Naver-Client-Secret", clientSecret)
+                .build();
+    }
+
+    @Bean
+    public WebClient anthropicWebClient() {
+        return WebClient.builder()
+                .baseUrl(claudeBaseUrl)
+                .defaultHeader("x-api-key", claudeApiKey)
+                .defaultHeader("anthropic-version", "2023-06-01")
+                .defaultHeader("content-type", "application/json")
                 .build();
     }
 }
